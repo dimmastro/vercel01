f8e8a2f9cef6694a4aa45ba629d4bbb9119639b4
dimmastro <k-d@yandex.ru>
2024-04-01 22:44:31 +0500

    funding table 2

--- server.js
+++ server.js
@@ -47,7 +47,29 @@ const runAll = async (factoryAddress, rpcEndpoint, connectionName) => {
             }
         );
 
+        const price = await client.queryContractSmart(marketAddress, {
+            spot_price: {},
+        });
+        console.log(JSON.stringify(price));
+
         const status = await client.queryContractSmart(marketAddress, { status: {} });
+
+        const doll= 100
+        const lev= 1
+        const fee_delta= 0
+        const fee_trade= 0.20 / 100 * doll
+        const fee_crank= 0.20 / 100 * doll
+        const short100= - status["long_funding"] * status["long_usd"] / (status["short_usd"]*1 + doll)
+        const short = short100
+
+        const sum1= ((- doll * lev * short / 365 - fee_crank ) * 1 - fee_trade).toFixed(2)
+        const sum7= ((- doll * lev * short / 365 - fee_crank ) * 7 - fee_trade).toFixed(2)
+        const sum30= ((- doll * lev * short  / 365 - fee_crank ) * 30 - fee_trade).toFixed(2)
+        const per1 = Math.round(sum1 / doll * 365 * 100)
+        const per7 = Math.round(sum7 / doll * 365 / 7 * 100)
+        const per30 = Math.round(sum30 / doll * 365 / 30 * 100)
+
+
 
         allStatus.push({
             chain: connectionName,
@@ -55,7 +77,26 @@ const runAll = async (factoryAddress, rpcEndpoint, connectionName) => {
             longFunding: Math.round(status["long_funding"]*100) ,
             shortFunding: Math.round(status["short_funding"]*100),
             longUSD: Math.round(status["long_usd"]),
-            shortUSD: Math.round(status["short_usd"])
+            shortUSD: Math.round(status["short_usd"]),
+            short100: Math.round(short100*100),
+
+            sum1: (sum1),
+            sum7: (sum7),
+            sum30: (sum30),
+            per1: (per1),
+            per7: (per7),
+            per30: (per30),
+
+            price_usd: (price["price_usd"]),
+            price_notional: (price["price_notional"]),
+            price_base: (price["price_base"]),
+
+            borrow_fee: (status["borrow_fee"]*10000)/10000,
+            instant_delta_neutrality_fee_value: (status["instant_delta_neutrality_fee_value"]*10000)/10000,
+            delta_neutrality_fee_fund: (status["delta_neutrality_fee_fund"]*10000)/10000,
+            fees_wallets: (status["fees"]["wallets"]*10000)/10000,
+            fees_protocol: (status["fees"]["protocol"]*10000)/10000,
+            fees_crank: (status["fees"]["crank"]*10000)/10000
         });
 
         // console.log(
@@ -69,15 +110,36 @@ const runAll = async (factoryAddress, rpcEndpoint, connectionName) => {
     return allStatus;
 };
 
+// const runAllForAllConnections = async () => {
+//     const allConnectionsStatus = [];
+//
+//     for (const connectionName in connections) {
+//         const connection = connections[connectionName];
+//         const status = await runAll(connection.factoryAddress, connection.rpcEndpoint, connectionName);
+//         // allConnectionsStatus[connectionName] = status;
+//         allConnectionsStatus.push(...status);
+//     }
+//
+//     return allConnectionsStatus;
+// };
+
 const runAllForAllConnections = async () => {
     const allConnectionsStatus = [];
 
-    for (const connectionName in connections) {
+    // Создание массива обещаний для каждого соединения
+    const promises = Object.keys(connections).map(async connectionName => {
         const connection = connections[connectionName];
         const status = await runAll(connection.factoryAddress, connection.rpcEndpoint, connectionName);
-        // allConnectionsStatus[connectionName] = status;
-        allConnectionsStatus.push(...status);
-    }
+        return status;
+    });
+
+    // Ожидание выполнения всех обещаний
+    const results = await Promise.all(promises);
+
+    // Объединение результатов
+    results.forEach(statusArray => {
+        allConnectionsStatus.push(...statusArray);
+    });
 
     return allConnectionsStatus;
 };
@@ -117,11 +179,34 @@ app.get('/api/html', (req, res) => {
 function generateHtmlTable(allConnectionsStatus) {
     let html = "";
     console.log(allConnectionsStatus);
-    html += "<style>table { border-collapse: collapse; } th, td { border: 1px solid black; }</style>";
+    html += "<style>table { border-collapse: collapse; } th, td { border: 1px solid black; text-align: right; }</style>";
 
     // html += `<h2>Funding</h2>`;
     // Добавление таблицы
-    html += "<table><thead><tr><th>Chain</th><th>Market</th><th>Long Funding</th><th>Short Funding</th><th>Long USD</th><th>Short USD</th></tr></thead><tbody>";
+    html += "<table><thead><tr><th>Chain</th><th>Market</th><th>Long Funding</th><th>Short Funding</th><th>Long USD</th>" +
+        "<th>Short USD</th>" +
+        "<th>short100</th>" +
+
+
+        "<th>sum1</th>" +
+        "<th>sum7</th>" +
+        "<th>sum30</th>" +
+        "<th>per1</th>" +
+        "<th>per7</th>" +
+        "<th>per30</th>" +
+
+        "<th>price_usd</th>" +
+        "<th>price_notional</th>" +
+        "<th>price_base</th>" +
+
+        "<th>borrow_fee</th>" +
+        "<th>instant_delta_neutrality_fee_value</th>" +
+        "<th>delta_neutrality_fee_fund</th>" +
+        "<th>fees_wallets</th>" +
+        "<th>fees_protocol</th>" +
+        "<th>fees_crank</th>" +
+        "</tr></thead><tbody>";
+
 
     // Сортировка массива по возрастанию shortFunding
     allConnectionsStatus.sort((a, b) => a.shortFunding - b.shortFunding);
@@ -130,7 +215,29 @@ function generateHtmlTable(allConnectionsStatus) {
     allConnectionsStatus.forEach(status => {
         // Добавление заголовка для каждой цепочки
         // Добавление строк таблицы для каждого элемента в цепочке
-        html += `<tr style="border: 1px solid black;"><td>${status.chain}</td><td>${status.market}</td><td>${status.longFunding}</td><td>${status.shortFunding}</td><td>${status.longUSD}</td><td>${status.shortUSD}</td></tr>`;
+        html += `<tr ><td>${status.chain}</td><td>${status.market}</td><td>${status.longFunding}</td><td>${status.shortFunding}</td><td>${status.longUSD}</td>
+<td>${status.shortUSD}</td>
+<td>${status.short100}</td>
+
+<td>${status.sum1}</td>
+<td>${status.sum7}</td>
+<td>${status.sum30}</td>
+<td>${status.per1}</td>
+<td>${status.per7}</td>
+<td>${status.per30}</td>
+
+
+<td>${status.price_usd}</td>
+<td>${status.price_notional}</td>
+<td>${status.price_base}</td>
+
+<td>${status.borrow_fee}</td>
+<td>${status.instant_delta_neutrality_fee_value}</td>
+<td>${status.delta_neutrality_fee_fund}</td>
+<td>${status.fees_wallets}</td>
+<td>${status.fees_protocol}</td>
+<td>${status.fees_crank}</td>
+</tr>`;
     });
     html += "</tbody></table>";
 
